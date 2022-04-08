1a2fa74144176f1d2a981ff15bfd4dc7f081c68a
M0nicah <monicawughanga@gmail.com>
2022-04-08 12:17:14 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'test-project';
+  title = 'My Goals';
+  // goals: string[];
+  // goals: Goal[] = [
+  //   { id: 1, name: 'Watch finding Nemo' },
+  //   { id: 2, name: 'Buy Cookies' },
+  //   { id: 3, name: 'Get new Phone Case' },
+  //   { id: 4, name: 'Get Dog Food' },
+  //   { id: 5, name: 'Solve math homework' },
+  //   { id: 6, name: 'Plot my world domination plan' },
+  // ];
+
+  constructor() {
+    // this.goals = ["Hire Ps", "Get Coffee", "Get to coding"];
+  }
+
+  ngOnInit(): void {}
 }
